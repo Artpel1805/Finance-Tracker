b10f56295b4eb6db8bd7aa6ecdb604ce54252ee6
ArtPel1805 <arthurpellegrin1104@gmail.com>
2024-03-11 11:00:58 +0100

    router

--- lib/app/navigation/routes.dart
+++ lib/app/navigation/routes.dart
@@ -1,13 +1,41 @@
-import 'package:flutter_artpel1805_template/app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:saving_goal/app/bloc/user/user_bloc.dart';
+import 'package:saving_goal/app/screens/create/create_screen.dart';
+import 'package:saving_goal/app/screens/home_page.dart';
+import 'package:saving_goal/component/colors.dart';
 
-// GoRouter configuration
+final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
+
+final GlobalKey<NavigatorState> _shellNavigatorKey =
+    GlobalKey<NavigatorState>();
+
 final router = GoRouter(
-  initialLocation: '/',
+  navigatorKey: _rootNavigatorKey,
+  initialLocation: '/home',
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-    ),
+    ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (context, state, child) => Scaffold(
+              resizeToAvoidBottomInset: false,
+              backgroundColor: FGTColors.black,
+              body: MultiBlocProvider(
+                providers: [BlocProvider(create: (_) => UserBloc())],
+                child: child,
+              ),
+            ),
+        routes: [
+          GoRoute(
+              path: '/home',
+              builder: (context, state) => const HomePage(),
+              routes: <RouteBase>[
+                GoRoute(
+                  path: 'create',
+                  builder: (context, state) => const CreateScreen(),
+                )
+              ]),
+        ])
+  
   ],
 );
